a235b041bc1877bba605fb39e65ce9210f4907fc
Fae <109899113+iamf-a-e@users.noreply.github.com>
2026-06-30 13:29:55 +0200

    Create index.dart

---
+++ lib/index.dart
@@ -0,0 +1,27 @@
+// Export pages
+export '/auth/login/login_widget.dart' show LoginWidget;
+export '/auth/complete_clincian_reg/complete_clincian_reg_widget.dart'
+    show CompleteClincianRegWidget;
+export '/auth/admin_create_account/admin_create_account_widget.dart'
+    show AdminCreateAccountWidget;
+export '/application/create_mom/create_mom_widget.dart' show CreateMomWidget;
+export '/application/home/home_widget.dart' show HomeWidget;
+export '/application/care_tools/care_tools_widget.dart' show CareToolsWidget;
+export '/application/ultrasound/ultrasound.dart' show UltrasoundApp;
+export '/application/moms/moms_widget.dart' show MomsWidget;
+export '/application/mom_details/mom_details_widget.dart' show MomDetailsWidget;
+export '/application/encounter/encounter_widget.dart' show EncounterWidget;
+export '/application/encounter_dets/encounter_dets_widget.dart'
+    show EncounterDetsWidget;
+export '/application/edit_encounter/edit_encounter_widget.dart'
+    show EditEncounterWidget;
+export '/application/scheduled_encounters/scheduled_encounters_widget.dart'
+    show ScheduledEncountersWidget;
+export '/reset_password/reset_password_widget.dart' show ResetPasswordWidget;
+export '/profile/profile_widget.dart' show ProfileWidget;
+export '/edit_profile/edit_profile_widget.dart' show EditProfileWidget;
+export '/auth/login_copy/login_copy_widget.dart' show LoginCopyWidget;
+export '/application/mom_details2/mom_details2_widget.dart'
+    show MomDetails2Widget;
+export '/application/patient_details/patient_details_widget.dart'
+    show PatientDetailsWidget;
